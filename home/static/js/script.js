65b6ede569ad48471838bc45b746541f1112021b
dori claudino <dori.claudino@gmail.com>
2018-06-27 19:08:50 -0400

    static files

---
+++ home/static/js/script.js
@@ -0,0 +1,90 @@
+/*
+
+Style   : MobApp Script JS
+Version : 1.0
+Author  : Surjith S M
+URI     : https://surjithctly.in/
+
+Copyright © All rights Reserved 
+
+*/
+
+$(function() {
+  "use strict";
+  /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+  function menuscroll() {
+    var $navmenu = $(".nav-menu.change-on-scrolling");
+
+    if ($(window).scrollTop() > 50) {
+      $navmenu.addClass("is-scrolling");
+    } else {
+      $navmenu.removeClass("is-scrolling");
+    }
+  }
+  menuscroll();
+  $(window).on("scroll", function() {
+    menuscroll();
+  });
+  /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+  $(".navbar-nav > li:not(.dropdown) > a").on("click", function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+  /* 
+     * NAVBAR TOGGLE BG
+     *-----------------*/
+  var siteNav = $("#navbar");
+  siteNav.on("show.bs.collapse", function(e) {
+    $(this)
+      .parents(".nav-menu")
+      .addClass("menu-is-open");
+  });
+  siteNav.on("hide.bs.collapse", function(e) {
+    $(this)
+      .parents(".nav-menu")
+      .removeClass("menu-is-open");
+  });
+
+  /*-----------------------------------
+     * OWL CAROUSEL
+     *-----------------------------------*/
+  var $testimonialsDiv = $(".testimonials");
+  if ($testimonialsDiv.length && $.fn.owlCarousel) {
+    $testimonialsDiv.owlCarousel({
+      items: 1,
+      nav: true,
+      dots: false,
+      navText: [
+        '<span class="ti-arrow-left"></span>',
+        '<span class="ti-arrow-right"></span>'
+      ]
+    });
+  }
+
+  var $galleryDiv = $(".img-gallery");
+  if ($galleryDiv.length && $.fn.owlCarousel) {
+    $galleryDiv.owlCarousel({
+      nav: false,
+      center: true,
+      loop: true,
+      autoplay: true,
+      dots: true,
+      navText: [
+        '<span class="ti-arrow-left"></span>',
+        '<span class="ti-arrow-right"></span>'
+      ],
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 3
+        }
+      }
+    });
+  }
+}); /* End Fn */
